446af20bc55894ca5f57e6c549bf27ffbba034b2
jisdulanjana <93591453+jisdulanjana@users.noreply.github.com>
2023-06-24 11:05:47 +0530

    add button

--- lib/home.dart
+++ lib/home.dart
@@ -10,6 +10,66 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Hello World');
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 19, 183, 201),
+          title: const Text('Calculator')),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Enter first number'),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter second number'),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Add'),
+                ),
+                const Spacer(),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Subtract'),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Multiply'),
+                ),
+                const Spacer(),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Divide'),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
