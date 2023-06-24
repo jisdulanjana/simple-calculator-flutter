89510f7180ca8c0c6ce7d4ef81874f1245cba0f1
jisdulanjana <93591453+jisdulanjana@users.noreply.github.com>
2023-06-24 12:44:13 +0530

    final output

--- lib/home.dart
+++ lib/home.dart
@@ -8,6 +8,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final TextEditingController firstContoller = TextEditingController();
+  final TextEditingController secondContoller = TextEditingController();
+  double result = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,17 +25,21 @@ class _HomePageState extends State<HomePage> {
             const SizedBox(
               height: 30,
             ),
-            const TextField(
-              decoration: InputDecoration(
+            TextField(
+              controller: firstContoller,
+              decoration: const InputDecoration(
                   border: OutlineInputBorder(), hintText: 'Enter first number'),
+              keyboardType: TextInputType.number,
             ),
             const SizedBox(
               height: 20,
             ),
-            const TextField(
-              decoration: InputDecoration(
+            TextField(
+              controller: secondContoller,
+              decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   hintText: 'Enter second number'),
+              keyboardType: TextInputType.number,
             ),
             const SizedBox(
               height: 20,
@@ -40,12 +48,24 @@ class _HomePageState extends State<HomePage> {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    double firstNumber = double.parse(firstContoller.text);
+                    double secondNumber = double.parse(secondContoller.text);
+                    setState(() {
+                      result = firstNumber + secondNumber;
+                    });
+                  },
                   child: const Text('Add'),
                 ),
                 const Spacer(),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    double firstNumber = double.parse(firstContoller.text);
+                    double secondNumber = double.parse(secondContoller.text);
+                    setState(() {
+                      result = firstNumber - secondNumber;
+                    });
+                  },
                   child: const Text('Subtract'),
                 ),
               ],
@@ -57,15 +77,34 @@ class _HomePageState extends State<HomePage> {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    double firstNumber = double.parse(firstContoller.text);
+                    double secondNumber = double.parse(secondContoller.text);
+                    setState(() {
+                      result = firstNumber * secondNumber;
+                    });
+                  },
                   child: const Text('Multiply'),
                 ),
                 const Spacer(),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    double firstNumber = double.parse(firstContoller.text);
+                    double secondNumber = double.parse(secondContoller.text);
+                    setState(() {
+                      result = firstNumber / secondNumber;
+                    });
+                  },
                   child: const Text('Divide'),
                 )
               ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              'Result: $result',
+              style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             )
           ],
         ),
